4b3ef93454842d1ead6de7a71bb58cecce5aae5e
Manish Kumar <manish041083@gmail.com>
2018-01-30 14:06:31 +0530

    Removing plugins which I don't need

--- zsh/commands.sh
+++ zsh/commands.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 `curl -L git.io/antigen > ~/.antigen.zsh`
+
+## Add command to install autojump here, by checking either port or brew is installed
--- zsh/plugins_configs.zsh
+++ zsh/plugins_configs.zsh
@@ -1,18 +1,4 @@
 
-### Jira Plugin configs:
-
-#export $JIRA_URL='https://jira.insnw.net'
-#export $JIRA_NAME='mkumar'
-#export $JIRA_PREFIX='PBL'
-#export $JIRA_RAPID_BOARD=true
-
-### ssh-agent configs:
-
-zstyle :omz:plugins:ssh-agent agent-forwarding on
-zstyle :omz:plugins:ssh-agent identities id_rsa id_rsa2 id_github
-#zstyle :omz:plugins:ssh-agent lifetime 4h
-#
-
 export FPATH="$FPATH:/opt/local/share/zsh/site-functions/"
 if [ -f /opt/local/etc/profile.d/autojump.sh ]; then
     . /opt/local/etc/profile.d/autojump.sh
