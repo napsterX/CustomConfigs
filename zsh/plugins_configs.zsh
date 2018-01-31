d3aefedf4233886d24cd7ff2a16ce7f7b02a6fc6
Manish Kumar <manish041083@gmail.com>
2018-01-31 10:40:37 +0530

    Disabling autojump for now

--- zsh/plugins_configs.zsh
+++ zsh/plugins_configs.zsh
@@ -1,5 +1,7 @@
 
-export FPATH="$FPATH:/opt/local/share/zsh/site-functions/"
-if [ -f /opt/local/etc/profile.d/autojump.sh ]; then
-    . /opt/local/etc/profile.d/autojump.sh
-fi
+
+## This will enable autojump if installed
+#export FPATH="$FPATH:/opt/local/share/zsh/site-functions/"
+#if [ -f /opt/local/etc/profile.d/autojump.sh ]; then
+#    . /opt/local/etc/profile.d/autojump.sh
+#fi
